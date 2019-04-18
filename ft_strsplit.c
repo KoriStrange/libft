86f54c3a2e335def0b180001e9ca777392abc517
KoriStrange <msefeane@gmail.com>
2019-04-18 19:54:00 +0200

    ft_strsplit timeout

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -1,74 +1,71 @@
 
 #include "libft.h"
 
-int	nwords(char *s, char c)
+int     nwords(char *s, char c)
 {
-	int i;
-	int j;
+        int i;
+        int j;
 
-	i = 0;
-	j = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-		if ((s[i] == c && s[i - 1] != c) || (s[i] == '\0' && s[i - 1] != c))
-			j++;
-	}
-	return (j);
-}
-/*
-int	nletters(char *s, char c)
+        i = 0;
+        j = 0;
+        while (s[i])
+        {
+                if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+                        j++;
+                i++;
+        }       
+        return (j);
+} 
+
+int     *nalpha(char *s, char c)
 {
-	int i;
-	int j;
-	int c;
-	int *n;
+        int i;
+        int j;
+        int k;
+        int *a;
 
-	i = 0;
-	j = 0;
-	c = 0;
-	n = (int *) malloc(sizeof(int) * nwords(s));
-	while (s[i] != '\0')
-	{
-		if (s[i] != c)
-			j++;
+        i = 0;
+        j = 0;
+        k = 0;
+	a = malloc(sizeof(int) * nwords(s, c));
+        while (s[i])
+        {
 		i++;
-		if ((s[i] == c && s[i - 1] != c) || (s[i] == '\0' && s[i - 1] != c))
+		if ((s[i] == c || s[i] == '\0') && s[i - 1] != c)
 		{
-			n[c] = j;
-			c++;
-			j = 0;
+		       	a[k] = i - j;
+			k++;
 		}
-	}
-	return (n);
+		if (s[i] == c)
+			j = i + 1;
+        }
+        return (a);
 }
-*/
-char	**ft_strplit(char *s, char c)
+
+char	**ft_strsplit(char *s, char c)
 {
 	int	i;
-	int	j;
-	int	k;
-	int	n;
+        int	j;
+        int	k;
+        int	*n;
 	char	**w;
 
-	i = 0;
-	j = 0;
-	k = 0;
-	n = 0;
+        i = 0;
+        j = 0;
+        k = 0;
+        n = nalpha(s, c);
 	w = (char **) malloc(sizeof(char *) * nwords(s, c));
-	while (s[i] != '\0')
-	{
-		if (s[i] != c)
-			j++;
-		i++;
-		if ((s[i] == c && s[i - 1] != c) || (s[i] == '\0' && s[i - 1] != c))
-		{
-			w[k] = ft_strsub(s, (unsigned int) n, (size_t) j);
-			k++;
-			j = 0;
-		}
-		if ((s[i + 1] != c && s[i] == c) || (s[i + 1] != '\0' && s[i] == c))
-			n = i + 1;
-	}
-	return (w);
+        while (s[i])
+        {
+                i++;
+                if ((s[i] == c || s[i] == '\0') && s[i - 1] != c)
+                {
+                        w[k] = ft_strnew(n[k] + 1);
+			w[k] = ft_strsub(s, j, n[k]);
+                        k++;
+                }
+                if (s[i] == c)
+                        j = i + 1;
+        }
+        return (w);
 }
